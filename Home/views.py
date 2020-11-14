ce231edc3567c063f343873ac4c8f9a0f9430906
Kamran Bhatti <kamranbhatti03072001@gmail.com>
2020-11-14 18:16:39 +0000

    fixed directory in Repo

--- Home/views.py
+++ Home/views.py
@@ -25,7 +25,7 @@ from django.views.decorators.csrf import csrf_exempt
 @csrf_exempt
 def update(request):
     if request.method == "POST":
-        repo = git.Repo("k5924.pythonanywhere.com/")
+        repo = git.Repo("Galaxy-Online/")
         origin = repo.remotes.origin
         origin.pull()
         return HttpResponse("Updated code on PythonAnywhere")
